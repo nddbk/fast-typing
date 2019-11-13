bce619c25541402055d5bd7ab75bb7ed1ef95bc5
Dong Nguyen <ndaidong@gmail.com>
2019-11-13 10:58:19 +0700

    Add textpad module

--- src/assets/js/modules/textpad.js
+++ src/assets/js/modules/textpad.js
@@ -1,3 +1,205 @@
 // modules -> textpad
 
+import {
+  queryAll,
+  get,
+  add,
+  Event,
+} from 'realdom';
+
+import {getTextString} from '../helpers/generator';
+import {onStarted, onPressed, onFinished} from '../helpers/analyzer';
+
+const $textpad = get('textpad');
+const $typingArea = get('typingArea');
+
+const state = {
+  isActivated: false,
+  text: '',
+  characters: [],
+  cursor: 0,
+  mistake: 0,
+  correct: 0,
+  totalChars: 0,
+  totalWords: 0,
+  startTime: 0,
+  endTime: 0,
+};
+
+const time = () => {
+  return (new Date()).getTime();
+};
+
+const start = () => {
+  state.startTime = time();
+  state.totalChars = state.text.length;
+  state.totalWords = state.text.split(' ').length;
+};
+
+const end = () => {
+  state.endTime = time();
+  onFinished(state);
+  restart();
+};
+
+const moveCursor = (k) => {
+  const {
+    characters,
+    mistake,
+  } = state;
+
+  if (k < 0 || k > characters.length) {
+    return false;
+  }
+
+  queryAll('.cursor').forEach((item) => {
+    item.removeClass('cursor');
+  });
+
+  if (k >= 0 && k < characters.length) {
+    const el = get('c_' + k);
+    el.addClass('cursor');
+    state.cursor = k;
+  }
+
+  if (k === 0 || k === 1 && mistake === 0) {
+    return start();
+  }
+  if (k === characters.length) {
+    return end();
+  }
+  return onPressed(state);
+};
+
+const moveBack = () => {
+  let {
+    cursor,
+    characters,
+  } = state;
+  if (cursor > 0) {
+    cursor--;
+    const prevChar = characters[cursor];
+    const el = get(prevChar.id);
+
+    if (el.hasClass('correct')) {
+      state.correct--;
+      el.removeClass('correct');
+    } else if (el.hasClass('error')) {
+      state.error--;
+      el.removeClass('error');
+    }
+    state.cursor = cursor;
+    moveCursor(cursor);
+  }
+};
+
+const moveNext = (char) => {
+  let {
+    cursor,
+    characters,
+  } = state;
+  if (cursor < characters.length) {
+    cursor++;
+    const prevChar = characters[cursor - 1];
+    const el = get(prevChar.id);
+    if (prevChar.char === char) {
+      el.addClass('correct');
+      state.correct++;
+    } else {
+      el.addClass('error');
+      state.error++;
+      state.mistake++;
+    }
+    state.cursor = cursor;
+    moveCursor(cursor);
+  }
+};
+
+const handleAction = (evt) => {
+  if (!state.isActivated) {
+    return false;
+  }
+  const e = evt || window.event;
+  const keyCode = e.keyCode || e.which;
+  if (keyCode === 8) {
+    Event.stop(evt);
+    return moveBack();
+  }
+  return e;
+};
+
+const handleText = (evt) => {
+  if (!state.isActivated) {
+    return false;
+  }
+  const e = evt || window.event;
+  const keyCode = e.keyCode || e.which;
+  const chr = String.fromCharCode(keyCode);
+  return moveNext(chr);
+};
+
+const reset = () => {
+  state.text = '';
+  state.characters = [];
+  state.cursor = 0;
+  state.startTime = 0;
+  state.endTime = 0;
+  state.error = 0;
+  state.correct = 0;
+  state.mistake = 0;
+  state.totalChars = 0;
+  state.totalWords = 0;
+  $typingArea.empty();
+};
+
+const render = (s) => {
+  reset();
+  state.text = s.trim();
+  const a = s.split('');
+  let i = 0;
+  a.forEach((c) => {
+    const id = 'c_' + i;
+    const span = add('SPAN', $typingArea);
+    span.id = id;
+    span.innerHTML = c;
+    state.characters.push({
+      index: i,
+      id,
+      char: c,
+    });
+    i++;
+  });
+  moveCursor(0);
+  return onStarted(state);
+};
+
+const load = (text) => {
+  const s = text || getTextString();
+  render(s);
+};
+
+const reactivate = () => {
+  $textpad.focus();
+  state.isActivated = true;
+};
+
+
+export const init = () => {
+  $textpad.focus();
+  Event.on(document.body, 'click', () => {
+    state.isActivated = false;
+    let focused = document.activeElement;
+    if (!focused || focused === document.body) {
+      focused = null;
+    } else if (document.querySelector) {
+      focused = document.querySelector(':focus');
+    }
+    if (!state.isActivated && focused && focused.id === 'textpad') {
+      state.isActivated = true;
+    }
+  });
+
+  Event.on(document.body, 'keydown', handleAction);
+  Event.on(document.body, 'keypress', handleText);
+};
 
